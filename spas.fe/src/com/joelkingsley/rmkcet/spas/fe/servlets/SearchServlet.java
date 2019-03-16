96480f261e06df3e99021a57493ba8ac31266be8
Joel Kingsley <joelkingsley.r@gmail.com>
2019-03-16 19:04:01 +0530

    Added add student

--- spas.fe/src/com/joelkingsley/rmkcet/spas/fe/servlets/SearchServlet.java
+++ spas.fe/src/com/joelkingsley/rmkcet/spas/fe/servlets/SearchServlet.java
@@ -33,8 +33,8 @@ public class SearchServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
+		
 	}
 
 }
