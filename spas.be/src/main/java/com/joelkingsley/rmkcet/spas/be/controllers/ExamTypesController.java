8727988761c41dbeb5952c48ea364e750ba7ad6b
MohammedAshi <98mohammedashik@gmail.com>
2019-03-26 22:00:53 +0530

    Added Add Examtype

--- spas.be/src/main/java/com/joelkingsley/rmkcet/spas/be/controllers/ExamTypesController.java
+++ spas.be/src/main/java/com/joelkingsley/rmkcet/spas/be/controllers/ExamTypesController.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joelkingsley.rmkcet.spas.be.beans.ExamType;
@@ -42,4 +44,20 @@ ResponseEntity<?> getAllExamTypes() {
 		return responseEntity;
 	}
 }
+
+
+@PostMapping("/examTypes")
+ResponseEntity<?> addexamtype(@RequestBody ExamType examType) {
+	 try { 
+		 ExamType addedExamType = examTypesDelegate.addExamType(examType); 
+		 ResponseEntity<ExamType> responseEntity = new ResponseEntity<ExamType>(addedExamType, HttpStatus.OK); 
+		 return responseEntity;
+
+	} catch (AppError appError) { 
+		appError.getException().printStackTrace(); 
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>(appError.getErrorMessage(), HttpStatus.INTERNAL_SERVER_ERROR); 
+		return responseEntity; 
+	}
 }
+}
+
--- spas.be/src/main/java/com/joelkingsley/rmkcet/spas/be/daos/ExamTypesDAO.java
+++ spas.be/src/main/java/com/joelkingsley/rmkcet/spas/be/daos/ExamTypesDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.joelkingsley.rmkcet.spas.be.beans.ExamType;
@@ -36,4 +38,42 @@ public class ExamTypesDAO {
 		}
 	}
 
-}
+	public ExamType addExamType(ExamType examType) throws AppError {
+
+		Connection connection = DBUtils.getConnection();
+		
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(DBQueries.ADD_EXAMTYPE, Statement.RETURN_GENERATED_KEYS);
+			preparedStatement.setString(1, examType.getExamTypeName());
+			preparedStatement.setString(2, examType.getAbbrevation());
+
+			int affectedRows = preparedStatement.executeUpdate();
+
+	        if (affectedRows == 0) {
+	            throw new AppError(ErrorConstants.EXAMTYPE_NOT_CREATED);
+	        }
+
+	        try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+	            if (generatedKeys.next()) {
+	                examType.setExamTypeID(generatedKeys.getInt(1));
+	                return examType;
+	            }
+	            else {
+	                throw new AppError(ErrorConstants.BATCH_ID_NOT_OBTAINED);
+	            }
+	        }
+			
+		} catch (SQLIntegrityConstraintViolationException exception) {
+			exception.printStackTrace();
+			throw new AppError(ErrorConstants.DUPLICATE_ENTRY, exception);
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+			throw new AppError(ErrorConstants.SERVER_ERROR, sqlException);
+		} catch (AppError appError) {
+			throw appError;
+		}
+	}
+
+	}
+
+
